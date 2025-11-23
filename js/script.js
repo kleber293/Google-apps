35d3b01d9653d96163d8b01d65dd4c8d93e46546
clebson_marques <70595783+kleber293@users.noreply.github.com>
2025-11-23 08:01:24 -0300

    Add logo image to Gmail entry in apps list

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
   const apps = [
     { n: 'Gmail', d: 'Email do Google', u: 'https://mail.google.com', 
-      i: '',
+      i: '<img srs="./logos/Gmail.png" >',
       tags: ['email', 'correio']
     },
     { n: 'Google Drive', d: 'Arquivos e pastas na nuvem', u: 'https://drive.google.com',
